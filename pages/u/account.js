b3e70bcfc38b2e008380f8fbdcd97c70272c0692
Caughell89 <CAughell89@yahoo.com>
2021-11-28 22:13:15 -0500

    adding all account info

--- pages/u/account.js
+++ pages/u/account.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useRef, useState } from "react";
 import { useUser } from "./../../utils/useUser";
+import moment from "moment";
 
 export default function Account() {
   const { userLoaded, user, session, userDetails, signOut } = useUser();
@@ -22,7 +23,17 @@ export default function Account() {
 
       <div className="content">
         <h1>Your Account</h1>
-        {userLoaded && <div>{user.email}</div>}
+        {userLoaded && (
+          <div>
+            <div>
+              <img src={user.user_metadata.avatar_url} alt="user profile pic" />
+              <div>{moment(user.created_at).format("LL")}</div>
+            </div>
+            <div>{user.user_metadata.full_name}</div>
+            <div>{user.email}</div>
+            <div>{user.phone}</div>
+          </div>
+        )}
       </div>
     </div>
   );
