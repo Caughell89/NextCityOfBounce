a1865faadb4b3e1249e2a5fc1a3ff0b4fc3f12d0
Caughell89 <CAughell89@yahoo.com>
2021-12-10 00:04:24 -0500

    updating babel and npm

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 // next.config.js
-const withAntdLess = require('next-plugin-antd-less');
-
+const withAntdLess = require("next-plugin-antd-less");
+const withImages = require("next-images");
+module.exports = withImages();
 module.exports = withAntdLess({
   // optional
-  modifyVars: { '@primary-color': '#1cacc8' },
+  modifyVars: { "@primary-color": "#1cacc8" },
   // optional
-  lessVarsFilePath: './src/styles/variables.less',
+  lessVarsFilePath: "./src/styles/variables.less",
   // optional
   lessVarsFilePathAppendToEndOfContent: false,
   // optional https://github.com/webpack-contrib/css-loader#object
